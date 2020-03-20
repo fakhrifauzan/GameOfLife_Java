21db34788e70803aa1f4724c22fe3378fcf79d37
Fakhri Fauzan <fakhri.fauzan@go-jek.com>
2020-03-20 15:56:12 +0700

    Modify countAliveNeighborCell in Board class to count alive diagonal top left neighbor

--- src/test/java/com/bootcamp/gameoflife/BoardTest.java
+++ src/test/java/com/bootcamp/gameoflife/BoardTest.java
@@ -103,4 +103,12 @@ public class BoardTest {
     board.setCellState(1, 2, true);
     Assert.assertEquals(1, board.countAliveNeighborCell(1, 1));
   }
+
+  @Test
+  public void givenOneAliveNeighborOnDiagonalTopLeftCountAliveNeighborCellShouldReturnOne() {
+    Board board = new Board(4, 4);
+    board.setCellState(1, 1, true);
+    board.setCellState(0, 0, true);
+    Assert.assertEquals(1, board.countAliveNeighborCell(1, 1));
+  }
 }
